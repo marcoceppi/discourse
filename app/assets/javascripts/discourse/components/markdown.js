789270eb5e86f8be90b0756c5af0dc734ba03f25
Marco Ceppi <marco@ceppi.net>
2013-06-18 18:59:00 -0400

    Check code set ups first

--- app/assets/javascripts/discourse/components/markdown.js
+++ app/assets/javascripts/discourse/components/markdown.js
@@ -142,7 +142,7 @@ Discourse.Markdown = {
 
     // Ubuntu and LP specific linking
     converter.hooks.chain("preConversion", function(text) {
-      return text.replace(/(apt|lp):([a-zA-Z0-9-]+)/gim, function(full, protocol, name) {
+      return text.replace(/(^|[\s\n])(apt|lp):([a-zA-Z0-9-]+)/gim, function(full, _, protocol, name) {
         if (protocol == "lp") {
           return "<a href=\"https://code.launchpad.net/" + name + "\">" + full + "</a>";
         } else {
